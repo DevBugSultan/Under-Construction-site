e13466f54ddd458838581e546ab5fd304db89884
DevBugSultan <devilsultan33@gmail.com>
2025-06-10 23:51:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ function App() {
    <div className="under-construction-container">
       <div className="text-center">
         <img 
-          src="/asset/done.png" 
+          src="/asset/dones.png" 
           alt="Under Construction" 
           className="construction-image"
         />
-        <h1 className="title">Site Under Construction</h1>
-        <p className="subtitle">Our website is under construction. Please try again later.</p>
+        <h1 className="title">Site Under Maintenance</h1>
+        <p className="subtitle">Our website is under maintenance. Please try again later.</p>
       </div>
     </div>
   );
